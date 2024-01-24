5c80be2a18dcbb822b73f7f9c7a93912cabed057
Neshbobo <wambuineema27@gmail.com>
2024-01-24 00:01:38 -0800

    6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as e:
-        print(f"Exception: {e}")
-        
+    raise NameError(message)
