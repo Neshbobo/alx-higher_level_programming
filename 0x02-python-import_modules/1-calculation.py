ee38f2b2e86a471158e06d8bac25405eea8c44bc
Neshbobo <wambuineema27@gmail.com>
2024-01-28 07:17:45 -0800

    1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 a = 10
 b = 5
+
 from calculator_1 import add, subtract, multiply, divide
-print(add(a, b))
-print(subtract(a, b))
-print(multiply(a, b))
-print(divide(a, b))
+
+add_result = add(a, b)
+sub_result = subtract(a, b)
+mul_result = multiply(a, b)
+div_result = divide(a, b)
+
+print("Addition result:", add_result)
+print("Subtraction result:", sub_result)
+print("Multiplication result:", mul_result)
+print("Division result:", div_result)
